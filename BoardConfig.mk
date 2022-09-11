a0cfa1a959e177f6854f6e2b079702c2af4d0c04
Rafael Méndez Pacheco <rafy.pachecomendez@gmail.com>
2022-09-11 16:16:48 -0300

    nabu: temporal change for testing the rebase kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,6 +9,8 @@ include device/xiaomi/sm8150-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/xiaomi/nabu
 
+NABU_KERNEL := true
+
 BUILD_BROKEN_DUP_RULES := true
 
 # Board
@@ -27,12 +29,21 @@ TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
 TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
 
+ifeq ($(NABU_KERNEL),true)
+BOARD_BOOT_HEADER_VERSION := 3
+KERNEL_LD := LD=ld.lld
+TARGET_KERNEL_ADDITIONAL_FLAGS := DTC_EXT=$(shell pwd)/prebuilts/misc/linux-x86/dtc/dtc
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_SOURCE := kernel/xiaomi/nabu
+TARGET_KERNEL_CONFIG := nabu_user_defconfig
+else
+TARGET_KERNEL_SOURCE := kernel/xiaomi/sm8150-legacy
+TARGET_KERNEL_CONFIG := vendor/sm8150-perf_defconfig vendor/xiaomi/sm8150-common.config vendor/xiaomi/nabu.config
+endif
+
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_nabu
 TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_nabu
-
-# Kernel
-TARGET_KERNEL_CONFIG += vendor/xiaomi/nabu.config
 
 # Inherit from the proprietary version
 include vendor/xiaomi/nabu/BoardConfigVendor.mk
