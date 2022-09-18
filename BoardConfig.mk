fa8ee2501edc877c08a6e0eb4ac31057a1169467
Rafael Méndez Pacheco <rafy.pachecomendez@gmail.com>
2022-09-18 19:56:22 -0300

    nabu: all variables are separated, and use again the kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,8 +9,9 @@ include device/xiaomi/sm8150-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/xiaomi/nabu
 
-NABU_KERNEL := false
-NABU_KERNEL_PREBUILT := true
+NABU_KERNEL_PREBUILT := false
+NABU_KERNEL := true
+COMMON_KERNEL := false
 
 BUILD_BROKEN_DUP_RULES := true
 
@@ -30,13 +31,7 @@ TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
 TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
 
-ifeq ($(NABU_KERNEL),true)
-BOARD_BOOT_HEADER_VERSION := 3
-#KERNEL_LD := LD=ld.lld
-TARGET_KERNEL_ADDITIONAL_FLAGS := DTC_EXT=$(shell pwd)/prebuilts/misc/linux-x86/dtc/dtc
-TARGET_KERNEL_SOURCE := kernel/xiaomi/nabu-kernel
-TARGET_KERNEL_CONFIG := nabu_user_defconfig
-else ($(NABU_KERNEL_PREBUILT),true)
+ifeq ($(NABU_KERNEL_PREBUILT),true)
 #vendor_cmdline
 VENDOR_CMDLINE := "console=ttyMSM0,115200n8 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 loop.max_part=7 cgroup.memory=nokmem,nosocket reboot=panic_warm androidboot.init_fatal_reboot_target=recovery androidboot.selinux=permissive androidboot.vbmeta.avb_version=1.0 androidboot.boot_devices=soc/1d84000.ufshc"
 #Kernel Variables
@@ -48,10 +43,18 @@ TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilts/$(PRODUCT_RELEASE_NAME)/kerne
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_MKBOOTIMG_ARGS += --vendor_cmdline $(VENDOR_CMDLINE)
 BOARD_MKBOOTIMG_ARGS += --pagesize $(BOARD_KERNEL_PAGESIZE) --board ""
-TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilts/$(PRODUCT_RELEASE_NAME)/dtb.img
-BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
 BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilts/$(PRODUCT_RELEASE_NAME)/dtbo.img
-else
+endif
+
+ifeq ($(NABU_KERNEL),true)
+BOARD_BOOT_HEADER_VERSION := 3
+#KERNEL_LD := LD=ld.lld
+TARGET_KERNEL_ADDITIONAL_FLAGS := DTC_EXT=$(shell pwd)/prebuilts/misc/linux-x86/dtc/dtc
+TARGET_KERNEL_SOURCE := kernel/xiaomi/nabu
+TARGET_KERNEL_CONFIG := nabu_user_defconfig
+endif
+
+ifeq ($(COMMON_KERNEL),true)
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sm8150-legacy
 TARGET_KERNEL_CONFIG += vendor/xiaomi/nabu.config
 endif
